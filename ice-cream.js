19bdcaa660c940f7e354b3f154369b624c2ed1f0
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-01 00:46:43 +0000

    New exercises

---
+++ ice-cream.js
@@ -0,0 +1,9 @@
+const vacationDays = 2 + 3;
+
+if (vacationDays <= 5) {
+  console.log("Not enough for a long vacation.");
+} else if (vacationDays === 5) {
+  console.log("We can take a whole week off, not bad.");
+} else {
+  console.log("We're gonna have a great time!");
+}
